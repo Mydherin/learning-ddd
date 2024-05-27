fb3fb8e300aa05b3565cb9b235eb1480a1928ede
jterrero <joseramonterrerolopez@gmail.com>
2024-05-27 21:26:29 +0200

    fix(rol): make constant public and static

--- 1-anemic-ddd/src/main/java/local/mydherin/users/domain/rol/services/IsShort.java
+++ 1-anemic-ddd/src/main/java/local/mydherin/users/domain/rol/services/IsShort.java
@@ -4,7 +4,7 @@ import local.mydherin.users.domain.rol.services.queries.IsShortQuery;
 
 public class IsShort {
 
-    private final Integer SHORT_NAME_LENGTH = 5;
+    public static final Integer SHORT_NAME_LENGTH = 5;
 
     public Boolean execute(final IsShortQuery query) {
         final var rol = query.getRol();
