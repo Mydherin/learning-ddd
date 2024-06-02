457639c316263905100f628e1aae9251b4c79614
jterrero <joseramonterrerolopez@gmail.com>
2024-06-02 23:28:08 +0200

    feat(game/saga): add base logic to SagaId object value

--- 1-anemic-ddd/src/main/java/local/mydherin/users/domain/game/entities/saga/vos/SagaId.java
+++ 1-anemic-ddd/src/main/java/local/mydherin/users/domain/game/entities/saga/vos/SagaId.java
@@ -3,4 +3,15 @@ package local.mydherin.users.domain.game.entities.saga.vos;
 import local.mydherin.users.domain.shared.ValueObject;
 
 public final class SagaId extends ValueObject {
+    private final String value;
+    private SagaId(final String value) {
+        this.value = value;
+        this.ensureIsNotNull(value);
+    }
+    public String getValue() {
+        return value;
+    }
+    public static SagaId of(final String value) {
+        return new SagaId(value);
+    }
 }
