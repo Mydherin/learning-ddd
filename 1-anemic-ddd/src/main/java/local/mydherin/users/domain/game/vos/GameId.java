cbda8c4f896558a80325d9a450fe9796925bc109
jterrero <joseramonterrerolopez@gmail.com>
2024-06-02 23:39:41 +0200

    feat(game): make gameId value object final

--- 1-anemic-ddd/src/main/java/local/mydherin/users/domain/game/vos/GameId.java
+++ 1-anemic-ddd/src/main/java/local/mydherin/users/domain/game/vos/GameId.java
@@ -2,7 +2,7 @@ package local.mydherin.users.domain.game.vos;
 
 import local.mydherin.users.domain.shared.ValueObject;
 
-public class GameId extends ValueObject {
+public final class GameId extends ValueObject {
     private final String value;
     private GameId(final String value) {
         this.value = value;
