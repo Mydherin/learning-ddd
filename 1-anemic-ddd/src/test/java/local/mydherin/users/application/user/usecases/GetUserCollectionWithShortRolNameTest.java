30d82455515db708eb8d34b0c6f5d42decfdea59
jterrero <joseramonterrerolopez@gmail.com>
2024-06-09 19:05:25 +0200

    test(user): make test case class final

--- 1-anemic-ddd/src/test/java/local/mydherin/users/application/user/usecases/GetUserCollectionWithShortRolNameTest.java
+++ 1-anemic-ddd/src/test/java/local/mydherin/users/application/user/usecases/GetUserCollectionWithShortRolNameTest.java
@@ -17,7 +17,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
-public class GetUserCollectionWithShortRolNameTest {
+public final class GetUserCollectionWithShortRolNameTest {
     @Mock
     private UserRepository userRepository;
     private GetUserCollectionWithShortRolName getUserCollectionWithShortRolName;
